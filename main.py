d01e61f91531372b4450bded3c49892e51e674bd
quietcorey <quietcorey@users.noreply.github.com>
2017-02-20 19:25:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def question_1():
     d) Collection
     """
     # Return the correct value.
-    pass
+    return 'Boolean'
 
 
 # Question 2
@@ -31,7 +31,7 @@ def question_2():
     e) "hello world"
     """
     # Return the correct value.
-    pass
+    return 'True'
 
 
 # Question 3
@@ -46,7 +46,13 @@ def remove_Es(a_string):
     remove_Es('abc')      # 'abc'
     """
     # Write your code here
-    pass
+    new_string = ''
+    for char in a_string:
+        if char != 'e' and char != 'E':
+            new_string += char
+    return new_string
+
+        
 
 
 # Question 4
@@ -63,7 +69,7 @@ def question_4():
     result = a_list[3**2 - 8] + a_list[-1] + a_tuple[2]
     """
     # Return the correct value.
-    pass
+    return 13
 
 
 # Question 5
@@ -94,7 +100,15 @@ def calculate_tax(income):
     calculate_tax(income)  # $210,000 * 0.35 = 73500 = $73,500
     """
     # Write your code here
-    pass
+    if income <= 50000:
+        return income * 0.15
+    elif income <= 75000:
+        return income * 0.25
+    elif income <= 100000:
+        return income * 0.30
+    else:
+        return income * 0.35
+        
 
 
 # Question 6
@@ -118,4 +132,8 @@ def matrix_sum(a_matrix):
     matrix_sum(m2)  # 198
     """
     # Write your code here
-    pass
+    sum = 0
+    for list in a_matrix:
+        for number in list:
+            sum += number
+    return sum
